d6a08cfe30e6df5d3611ab7ce1d92e797a736e35
Aleksander Dukaczewski <aleksander.dukaczewski1@gmail.com>
2021-08-02 04:53:37 +0200

    added a pseudorandom maze generator

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import generate from "generate-maze";
+
 export const defaultState = () => {
     return {
         x: 1,
@@ -5,7 +7,10 @@ export const defaultState = () => {
         gameFinished: false,
         isRunning: true,
         score: 0,
+        maze: generate(20, 20, true, randomInt(10000)),
     };
 };
 
 export const canMoveTo = (x, y) => (x >= 1 && y >= 1 && x <= 20 && y <= 20);
+
+const randomInt = (max) => Math.floor(Math.random() * max);
